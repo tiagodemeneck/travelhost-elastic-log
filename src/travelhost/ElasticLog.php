7842bf478a212ed601f30f3ed57ef9d8f43691b2
Tiago Reis Demeneck <tiago.demeneck@travelhost.com.br>
2024-06-26 12:47:32 -0300

    second commit

--- src/travelhost/ElasticLog.php
+++ src/travelhost/ElasticLog.php
@@ -2,10 +2,24 @@
 
 namespace Travelhost;
 
+use Elastic\Elasticsearch\Client;
+use Elastic\Elasticsearch\ClientBuilder;
+
 class ElasticLog{
 
+    protected $client;
+    public function __construct()
+    {
+        $this->client = ClientBuilder::create()->setHosts([env('ELASTICSEARCH_HOST')])->build();           
+    }
     public function teste(){
         return "teste";
+    }
+
+    public function budgetActionsReport(string $indexName){
+
+        $logs = $this->client->search(['indexName' => $indexName]);
+        return $logs;
     }
 }
 
